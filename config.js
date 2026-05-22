a88c34c6bc07090dda7a8a91f269d49b46f1c19f
abenaakakesara-arch <abenaakakesara@gmail.com>
2026-05-22 16:13:59 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+module.exports = {
+
+botname: process.env.BOT_NAME || "DARK-QUEEN",
+ownername: process.env.OWNER_NAME || "KESARA",
+prefix: process.env.PREFIX || "."
+
+};
